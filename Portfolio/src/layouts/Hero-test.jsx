536ec5f78dd624cb5d91d82a4dc35348cc931b6f
mariosantos2003-dot <mariosantosbl2003@gmail.com>
2025-03-07 12:10:31 +0100

    add Skills section and update favicon to logo

--- Portfolio/src/layouts/Hero-test.jsx
+++ Portfolio/src/layouts/Hero-test.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
                 <h2 className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 text-7xl">
                     FrontEnd Developer
                 </h2>
-                <h3 className='mt-10 text-4xl'>Transforming Ideas To Reality</h3>
+                
             </section>
 
             {/* Main Content Section */}
